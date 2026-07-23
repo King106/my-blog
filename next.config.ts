d51d50aba0fa03699bb5adfb8d90097690222332
w_lijingxuan <w_lijingxuan@kingsoft.com>
2026-07-23 20:38:56 +0800

    配置 GitHub Pages 静态部署

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/my-blog",
+  assetPrefix: "/my-blog/",
 };
 
 export default nextConfig;
